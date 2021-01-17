754efe2bad1028144cd3a4f04724616bb2344786
umarcor <unai.martinezcorral@ehu.eus>
2021-01-17 01:39:05 +0100

    use GHDL_PREFIX instead of GHDL_LIBS

--- verification/UVVM/run.ps1
+++ verification/UVVM/run.ps1
@@ -1,2 +1,2 @@
 git submodule update --init --recursive verification\UVVM\uvvm
-& $env:GHDL_LIBS\vendors\compile-uvvm.ps1 -UVVM -UVVM_VIP_UART -Source verification\UVVM\uvvm -Output precompiled
+& $env:GHDL_PREFIX\vendors\compile-uvvm.ps1 -UVVM -UVVM_VIP_UART -Source verification\UVVM\uvvm -Output precompiled
