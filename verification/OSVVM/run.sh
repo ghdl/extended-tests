a83ea5b7ba087b7e4ec2aba3f249dc8e21bed17b
umarcor <unai.martinezcorral@ehu.eus>
2021-01-24 06:33:17 +0100

    uvvm: enable scoreboard and uart in bash scripts

--- verification/OSVVM/run.sh
+++ verification/OSVVM/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
 git submodule update --init --recursive verification/OSVVM/OsvvmLibraries
-$GHDL_PREFIX/vendors/compile-osvvm.sh --osvvm --source $(dirname $0)/OsvvmLibraries/osvvm --output precompiled
+$GHDL_PREFIX/vendors/compile-osvvm.sh \
+  --osvvm \
+  --source $(dirname $0)/OsvvmLibraries/osvvm --output precompiled
--- verification/UVVM/run.sh
+++ verification/UVVM/run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
 git submodule update --init --recursive verification/UVVM/uvvm
-$GHDL_PREFIX/vendors/compile-uvvm.sh --uvvm --source $(dirname $0)/uvvm --output precompiled
+$GHDL_PREFIX/vendors/compile-uvvm.sh \
+  --uvvm \
+  --uvvm-vip-scoreboard \
+  --uvvm-vip-uart \
+  --source $(dirname $0)/uvvm --output precompiled
